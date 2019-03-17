a4f605f8db2e28c73a628724215ff74ac679944c
“Allie <allieroll22@ufl.edu>
2019-03-17 15:39:00 -0400

    Add basic style to CSS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 // It is always helpful to use comments in your code!
+const boilingTemp = '212';
+const freezingTemp = '32';
+const waterTemp = '100';
+console.log(boilingTemp);
+console.log(freezingTemp);
+console.log(waterTemp);
