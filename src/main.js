7ff33820f4def8896bc432e95a2e712da960f2a6
“Allie <allieroll22@ufl.edu>
2019-03-17 17:05:00 -0400

    Add JS temp functions

--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
 // It is always helpful to use comments in your code!
-const boilingTemp = '212';
-const freezingTemp = '32';
-const waterTemp = '100';
-console.log(boilingTemp);
-console.log(freezingTemp);
-console.log(waterTemp);
+//const boilingTemp = document.createTextNode(' 212');
+//const freezingTemp = document.createTextNode('32');
+//const waterTemp = document.createTextNode('100');
+
+const boilingSpan = document.getElementById('boiling-temp');
+const boilingTemp = document.createTextNode(' 212');
+const boiling = () => boilingSpan.appendChild(boilingTemp);
+
+const freezingSpan = document.getElementById('freezing-temp');
+const freezingTemp = document.createTextNode('32')
+const freezing = () => freezingSpan.appendChild(freezingTemp);
+
+
+const waterSpan = document.getElementById('water-temp');
+const waterTemp = document.createTextNode('300')
+const water = () => waterSpan.appendChild(waterTemp);
+
+let hotTemp = document.getElementById('good-temp');
+
+setTimeout(function(){
+  if (waterTemp > 212){
+    hotTemp.classList.remove("hide").add("show");
+  }
+
+}, 3000);
+/*
+
+ boilingSpan.appendChild(boilingTemp);
+  freezingSpan.appendChild(freezingTemp);
+  waterSpan.appendChild(waterTemp);
+
+let goodTemp = document.getElementById('good-temp');
+setTimeout(functionRun, 3000);
+
+*/
